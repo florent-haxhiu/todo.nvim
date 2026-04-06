e13f0ed9ea86e1b54345a444ea820d839782b605
Florent Haxhiu <florenthaxhiu03@gmail.com>
2026-04-06 19:34:42 +0100

    feat(cmds): Todo* to run different commands

--- plugin/todo.lua
+++ plugin/todo.lua
@@ -1,9 +1,17 @@
---- Test todo 
-vim.api.nvim_create_user_command("TodoStart", function()
-	require("todo").print_hello_world()
+--- Test todo
+vim.api.nvim_create_user_command("Todo", function()
+	vim.api.nvim_create_buf(false, true)
 end, {})
 
 --- Add todo
 vim.api.nvim_create_user_command("TodoAdd", function(cmd_opts)
 	require("todo").add_todo({ title = cmd_opts.args })
 end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("TodoView", function()
+	require("todo").view_todos()
+end, {})
+
+vim.api.nvim_create_user_command("TodoComplete", function(cmd_opts)
+	require("todo").complete_todo({ title = cmd_opts.args })
+end, {})
